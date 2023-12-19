1ffbaf2c96a759661923a3cab5025221b5950bb1
felmateos <felmateos@usp.br>
2023-12-19 18:51:55 -0300

    added ratings evaluation

---
+++ app/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title='meu site streamlit')
+
+st.subheader('meu primeiro site com streamlit')
+
+st.title('suco de fruta')
+
+st.write('suco de fruta eh top')
+
+st.write('quer ver meu github? [clique aqui](https://github.com/felmateos/)')
